bd2fa602ac6febaa2699def3f2d341093a8172d3
ksiki <j.kolyasha@gmail.com>
2026-05-20 15:15:21 +0300

    feat: master parser DAG

---
+++ airflow/dags/master_parser_dag.py
@@ -0,0 +1,87 @@
+import logging
+import os
+from datetime import datetime, timedelta
+from typing import Any
+
+from airflow.decorators import dag, task
+from airflow.providers.docker.operators.docker import DockerOperator
+from airflow.providers.http.hooks.http import HttpHook
+from docker.types import Mount
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+DEFAULT_ARGS = {
+    "owner": "whilework",
+    "depends_on_past": False,
+    "retries": 2,
+    "retry_delta": timedelta(minutes=5),
+}
+
+
+@dag(
+    dag_id="master_parser_orchestrator",
+    default_args=DEFAULT_ARGS,
+    schedule="*/5 * * * *",
+    start_date=datetime(2026, 5, 20),
+    catchup=False,
+    max_active_runs=1,
+)
+def master_parser_dag() -> None:
+    @task(multiple_outputs=False)
+    def fetch_active_sources() -> list[dict[str, Any]]:
+        internal_token = os.getenv("INTERNAL_API_TOKEN")
+        headers = {"X-Internal-Secret": internal_token}
+
+        http_hook = HttpHook(http_conn_id="internal_api", method="GET")
+        endpoint = "/api/internal/v1"
+
+        response = http_hook.run(endpoint=f"{endpoint}/sources/", headers=headers)
+
+        records = response.json()
+        if not records:
+            logger.warning("Sources list is empty")
+            return []
+
+        env_list = []
+        for row in records:
+            source_id = str(row.get("id"))
+            platform = str(row.get("platform"))
+            identifier = str(row.get("identifier"))
+            last_parsed_id = row.get("last_parsed_id")
+            last_parsed_id = str(last_parsed_id) if last_parsed_id is not None else None
+
+            env_list.append(
+                {
+                    "SOURCE_ID": source_id,
+                    "PLATFORM": platform,
+                    "IDENTIFIER": identifier,
+                    "LAST_PARSED_ID": last_parsed_id,
+                    "INTERNAL_API_TOKEN": internal_token,
+                    "INTERNAL_BACKEND_URL": f"{http_hook.host}{endpoint}",
+                }
+            )
+
+        return env_list
+
+    active_sources_envs = fetch_active_sources()
+
+    run_parsers = DockerOperator.partial(
+        task_id="run_isolated_parser",
+        image="whilework-app-parser:latest",
+        api_version="auto",
+        auto_remove="success",
+        network_mode="backend_network",
+        docker_url="unix://var/run/docker.sock",
+        pool="docker_parsers_pool",
+        mounts=[
+            Mount(source="/opt/whilework/sessions", target="/app/sessions", type="bind")
+        ],
+    ).expand(environment=active_sources_envs)
+
+    active_sources_envs >> run_parsers
+
+
+dag_instance = master_parser_dag()
