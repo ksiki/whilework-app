dc180219c12cd718fc1b8ac37d1fc78f08d4e71c
ksiki <j.kolyasha@gmail.com>
2026-05-21 18:27:55 +0300

    fix: add all admin models in __init__

--- backend/apps/vacancies/admin/__init__.py
+++ backend/apps/vacancies/admin/__init__.py
@@ -1,5 +1,15 @@
 from .company import CompanyAdmin
+from .contact import ContactAdmin
+from .location import LocationAdmin
+from .skill import SkillAdmin
+from .vacancy import Vacancy
+from .work_format import WorkFormatAdmin
 
 __all__ = [
     "CompanyAdmin",
+    "ContactAdmin",
+    "LocationAdmin",
+    "SkillAdmin",
+    "WorkFormatAdmin",
+    "Vacancy",
 ]
