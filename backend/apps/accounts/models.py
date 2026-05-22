e95eecd7fc24cadf285ea09aa29fbfbc2f92d0a5
ksiki <j.kolyasha@gmail.com>
2026-05-22 10:27:06 +0300

    refactor: chenges a little error in accounts models

--- backend/apps/accounts/models.py
+++ backend/apps/accounts/models.py
@@ -27,24 +27,24 @@ class User(TimeStampedMixin):
         ordering = ["created_at"]
 
     def __str__(self):
-        return f"{self.email}, register: {self.created_at}, available slots: {self.available_slots}, last activity: {self.last_active_at}"
+        return f"{self.email}, register: {self.created_at}, available slots: {self.available_slots}"
 
 
 class Notification(TimeStampedMixin):
-    d = models.UUIDField(
+    id = models.UUIDField(
         primary_key=True,
         default=uuid.uuid4,
         editable=False,
         verbose_name="Notification ID",
     )
-    user = models.ForeignKey(
-        "User", on_delete=models.CASCADE, related_name="notifications"
+    users = models.ManyToManyField(
+        "User",
+        through="UserNotification",
+        related_name="notifications",
     )
 
     title = models.CharField(max_length=255)
     message = models.TextField()
-
-    is_read = models.BooleanField(default=False)
 
     class Meta:
         db_table = "accounts_notification"
@@ -53,14 +53,50 @@ class Notification(TimeStampedMixin):
         ordering = ["created_at"]
 
     def __str__(self):
-        return f"iser id: {self.user_id}, created at: {self.created_at}, is read: {self.is_read}"
+        return self.title
+
+
+class UserNotification(models.Model):
+    """
+    An intermediate table for tracking the read status of a notification for each user
+    """
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        verbose_name="User Notification ID",
+    )
+    user = models.ForeignKey(
+        "User", on_delete=models.CASCADE, related_name="user_notifications"
+    )
+    notification = models.ForeignKey(
+        Notification, on_delete=models.CASCADE, related_name="user_notifications"
+    )
+
+    is_read = models.BooleanField(default=False)
+
+    read_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        db_table = "accounts_user_notification"
+        verbose_name = "User Notification"
+        verbose_name_plural = "User Notifications"
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "notification"], name="unique_user_notification"
+            )
+        ]
+
+    def __str__(self):
+        return f"user: {self.user.email}, notification id: {self.notification_id}, read: {self.is_read}"
 
 
 class SlotTransaction(TimeStampedMixin):
     class PaymentStatus(models.TextChoices):
-        SECCESSFULLY = "SCS", "Seccessfully"
-        EXPECTATION = "EXP", "Expectation"
-        CANCEL = "CNC", "Cancel"
+        SUCCESS = "SCS", "Success"
+        PENDING = "PND", "Pending"
+        CANCELLED = "CNC", "Cancelled"
 
     id = models.UUIDField(
         primary_key=True,
@@ -77,13 +113,18 @@ class SlotTransaction(TimeStampedMixin):
     )
 
     slots_added = models.IntegerField(help_text="Number of slots purchased")
-    payment_status = models.CharField(max_length=3, choices=PaymentStatus.choices)
+    payment_status = models.CharField(
+        max_length=3,
+        choices=PaymentStatus.choices,
+        default=PaymentStatus.PENDING,
+    )
 
     class Meta:
         db_table = "accounts_slot_transaction"
         verbose_name = "Slot Transaction"
         verbose_name_plural = "Slot Transactions"
-        ordering = ["created_at"]
+        ordering = ["-created_at"]
 
     def __str__(self):
-        return f"iser id: {self.user_id}, created at: {self.created_at}, is read: {self.is_read}"
+        user_email = self.user.email if self.user else "Deleted User"
+        return f"email: {user_email}, slots: +{self.slots_added}"
