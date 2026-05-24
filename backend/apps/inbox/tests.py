457a8b50a1f991dcc06e1f84a07a4db307a0ba7c
ksiki <j.kolyasha@gmail.com>
2026-05-24 18:51:38 +0300

    refactor: tests for inbox and sources

--- backend/apps/inbox/tests.py
+++ backend/apps/inbox/tests.py
@@ -2,7 +2,7 @@ from typing import Any
 from unittest.mock import patch
 
 import pytest
-from tests.factories import SOURCE_1_ID, SOURCE_2_ID
+from tests.factories import UUID_1_ID, UUID_2_ID, SourceFactory
 
 from apps.inbox.models import ParserRawMessage
 from apps.inbox.services import (
@@ -15,24 +15,30 @@ from apps.inbox.services import (
 def sample_messages_data() -> list[dict[str, Any]]:
     return [
         {
-            "source_id": SOURCE_1_ID,
+            "source_id": UUID_1_ID,
             "external_msg_id": "msg_100",
             "raw_text": "Ищем Python Backend разработчика",
             "metadata": {"author": "HR"},
         },
         {
-            "source_id": SOURCE_1_ID,
+            "source_id": UUID_1_ID,
             "external_msg_id": "msg_101",
             "raw_text": "Вакансия Data Engineer",
             "metadata": {},
         },
         {
-            "source_id": SOURCE_2_ID,
+            "source_id": UUID_2_ID,
             "external_msg_id": "post_55",
             "raw_text": "Middle Django Developer",
             "metadata": {"views": 150},
         },
     ]
+
+
+@pytest.fixture
+def setup_sources(db) -> None:
+    SourceFactory(id=UUID_1_ID, is_active=True)
+    SourceFactory(id=UUID_2_ID, is_active=True)
 
 
 def test_create_object_list_and_search_last_ids(sample_messages_data) -> None:
@@ -41,13 +47,13 @@ def test_create_object_list_and_search_last_ids(sample_messages_data) -> None:
     assert len(result.instances) == 3
     assert all(isinstance(obj, ParserRawMessage) for obj in result.instances)
 
-    assert result.instances[0].source_id == SOURCE_1_ID
+    assert result.instances[0].source_id == UUID_1_ID
     assert result.instances[0].external_msg_id == "msg_100"
     assert result.instances[0].raw_text == "Ищем Python Backend разработчика"
 
     assert dict(result.latest_parsed_ids) == {
-        SOURCE_1_ID: "msg_101",
-        SOURCE_2_ID: "post_55",
+        UUID_1_ID: "msg_101",
+        UUID_2_ID: "post_55",
     }
 
 
@@ -62,7 +68,7 @@ def test_atomic_saved_messages_and_update_sources(
 
     saved_msg = ParserRawMessage.objects.get(external_msg_id="msg_100")
     assert saved_msg.status == "PND"
-    assert saved_msg.source_id == SOURCE_1_ID
+    assert saved_msg.source_id == UUID_1_ID
 
     atomic_saved_messages_and_update_sources(sample_messages_data)
     assert ParserRawMessage.objects.count() == 3
@@ -72,6 +78,6 @@ def test_atomic_saved_messages_and_update_sources(
     called_kwargs = mock_update_sources.call_args.kwargs
     assert "new_last_ids" in called_kwargs
     assert dict(called_kwargs["new_last_ids"]) == {
-        SOURCE_1_ID: "msg_101",
-        SOURCE_2_ID: "post_55",
+        UUID_1_ID: "msg_101",
+        UUID_2_ID: "post_55",
     }
--- backend/apps/sources/tests.py
+++ backend/apps/sources/tests.py
@@ -1,8 +1,9 @@
 import uuid
 from types import MappingProxyType
+from typing import Final
 
 import pytest
-from tests.factories import SOURCE_1_ID, SOURCE_3_ID
+from tests.factories import UUID_1_ID, UUID_2_ID, SourceFactory
 
 from .models import Source
 from .services import (
@@ -11,7 +12,14 @@ from .services import (
     update_last_parsed_message_ids,
 )
 
-NOT_EXISTS_ID = uuid.UUID("44444444-4444-4444-a444-444444444444")
+NOT_EXISTS_ID: Final[uuid.UUID] = uuid.UUID("44444444-4444-4444-a444-444444444444")
+
+
+@pytest.fixture
+def setup_sources(db) -> None:
+    SourceFactory(id=UUID_1_ID, error_count=3, is_active=True)
+    SourceFactory(is_active=False)
+    SourceFactory(id=UUID_2_ID, is_active=True)
 
 
 @pytest.mark.django_db
@@ -19,43 +27,42 @@ def test_get_active_sources(setup_sources) -> None:
     result = get_active_sources()
 
     assert len(result) == 2
-    assert result[0].id == SOURCE_1_ID
-    assert result[1].id == SOURCE_3_ID
+    assert result[0].id == UUID_1_ID
+    assert result[1].id == UUID_2_ID
 
 
 @pytest.mark.django_db
 def test_update_last_message_ids(setup_sources) -> None:
-    source = Source.objects.get(id=SOURCE_1_ID)
-
+    source = Source.objects.get(id=UUID_1_ID)
     assert source.last_parsed_id is None
 
-    data = MappingProxyType({SOURCE_1_ID: "999"})
+    data = MappingProxyType({UUID_1_ID: "999"})
     update_last_parsed_message_ids(new_last_ids=data)
-    updated_source = Source.objects.get(id=SOURCE_1_ID)
 
-    assert updated_source.last_parsed_id == "999"
+    source.refresh_from_db()
+    assert source.last_parsed_id == "999"
 
 
 @pytest.mark.django_db
 def test_register_source_error(setup_sources) -> None:
-    source = Source.objects.get(id=SOURCE_1_ID)
+    source = Source.objects.get(id=UUID_1_ID)
 
     assert source.error_count == 3
     assert source.last_error_message is None
 
-    register_source_error(source_id=SOURCE_1_ID, error_message="Error_1")
-    source = Source.objects.get(id=SOURCE_1_ID)
+    register_source_error(source_id=UUID_1_ID, error_message="Error_1")
+    source = Source.objects.get(id=UUID_1_ID)
 
     assert source.error_count == 4
     assert source.last_error_message == "Error_1"
     assert source.is_active is True
 
-    register_source_error(source_id=SOURCE_1_ID, error_message="Error_2")
-    source = Source.objects.get(id=SOURCE_1_ID)
+    register_source_error(source_id=UUID_1_ID, error_message="Error_2")
+    source = Source.objects.get(id=UUID_1_ID)
 
     assert source.error_count == 5
     assert source.last_error_message == "Error_2"
     assert source.is_active is False
 
     with pytest.raises(Source.DoesNotExist):
-        assert register_source_error(source_id=NOT_EXISTS_ID, error_message="Error_3")
+        register_source_error(source_id=NOT_EXISTS_ID, error_message="Error_3")
