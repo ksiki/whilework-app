132624953fab581f73f519d66a56b6d6d165ae6a
ksiki <j.kolyasha@gmail.com>
2026-05-26 17:29:58 +0300

    test: update conftest and add new factories

--- backend/conftest.py
+++ backend/conftest.py
@@ -1,12 +1,20 @@
 from pytest_factoryboy import register
 from tests.factories import (
     CompanyFactory,
+    ContactFactory,
     LocationFactory,
+    SkillFactory,
     SourceFactory,
+    UserFactory,
     VacancyFactory,
+    WorkFormatFactory,
 )
 
 register(SourceFactory)
 register(CompanyFactory)
 register(LocationFactory)
 register(VacancyFactory)
+register(UserFactory)
+register(ContactFactory)
+register(WorkFormatFactory)
+register(SkillFactory)
--- backend/tests/factories.py
+++ backend/tests/factories.py
@@ -3,19 +3,26 @@ from datetime import datetime
 from typing import Final
 
 import factory
+from apps.accounts.models import User
 from apps.sources.models import Source
-from apps.vacancies.models import Company, Location, Vacancy
+from apps.vacancies.models import Company, Contact, Location, Skill, Vacancy, WorkFormat
+from django.contrib.auth.hashers import make_password
 from factory.django import DjangoModelFactory
 from faker import Faker
 
 fake = Faker("ru_RU")
-
 
 UUID_1_ID: Final[uuid.UUID] = uuid.UUID("11111111-1111-1111-a111-111111111111")
 UUID_2_ID: Final[uuid.UUID] = uuid.UUID("22222222-2222-2222-a222-222222222222")
 UUID_3_ID: Final[uuid.UUID] = uuid.UUID("22232223-2223-2223-a333-222322232223")
 UUID_4_ID: Final[uuid.UUID] = uuid.UUID("22242224-2224-2224-a444-222422242224")
 UUID_5_ID: Final[uuid.UUID] = uuid.UUID("22252225-2225-2225-a555-222522252225")
+
+INT_1_ID: Final[int] = 1
+INT_2_ID: Final[int] = 2
+INT_3_ID: Final[int] = 3
+INT_4_ID: Final[int] = 4
+INT_5_ID: Final[int] = 5
 
 
 class SourceFactory(DjangoModelFactory):
@@ -37,6 +44,22 @@ class CompanyFactory(DjangoModelFactory):
     slug = factory.Sequence(lambda n: f"company-{n}")
 
 
+class SkillFactory(DjangoModelFactory):
+    class Meta:
+        model = Skill
+
+    name = factory.Faker("skill")
+    slug = factory.Sequence(lambda n: f"skill-{n}")
+
+
+class WorkFormatFactory(DjangoModelFactory):
+    class Meta:
+        model = WorkFormat
+
+    name = factory.Faker("work-format")
+    slug = factory.Sequence(lambda n: f"work-format-{n}")
+
+
 class LocationFactory(DjangoModelFactory):
     class Meta:
         model = Location
@@ -44,6 +67,14 @@ class LocationFactory(DjangoModelFactory):
     region = "Europe"
     country = "Czech Republic"
     city = "Prague"
+
+
+class ContactFactory(DjangoModelFactory):
+    class Meta:
+        model = Contact
+
+    platform = "TG"
+    details = "@example"
 
 
 class VacancyFactory(DjangoModelFactory):
@@ -55,3 +86,34 @@ class VacancyFactory(DjangoModelFactory):
     location = factory.SubFactory(LocationFactory)
     source = factory.SubFactory(SourceFactory)
     published_at = datetime.now()
+
+    @factory.post_generation
+    def skills(self, create, extracted, **kwargs):
+        if not create or not extracted:
+            return
+        for skill in extracted:
+            self.skills.add(skill)
+
+    @factory.post_generation
+    def work_formats(self, create, extracted, **kwargs):
+        if not create or not extracted:
+            return
+        for work_format in extracted:
+            self.work_formats.add(work_format)
+
+
+class UserFactory(DjangoModelFactory):
+    class Meta:
+        model = User
+
+    email = factory.Faker("email")
+    password = factory.LazyFunction(lambda: make_password("password"))
+    available_slots = 0
+
+    @factory.post_generation
+    def company_blacklist(self, create, extracted, **kwargs):
+        if not create or not extracted:
+            return
+
+        for company in extracted:
+            self.company_blacklist.add(company)
