23f8111954e3ab15cce7fe07b084b7693459d51b
ksiki <j.kolyasha@gmail.com>
2026-05-28 19:55:05 +0300

    feat: add logging

--- backend/apps/inbox/views.py
+++ backend/apps/inbox/views.py
@@ -1,9 +1,13 @@
+import logging
+
 from django.http import HttpRequest
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .services import atomic_saved_messages_and_update_sources
+
+logger = logging.getLogger(__name__)
 
 
 class ReceiveRawBatchView(APIView):
@@ -20,10 +24,11 @@ class ReceiveRawBatchView(APIView):
         try:
             atomic_saved_messages_and_update_sources(messages_data=messages_data)
             return Response(
-                {"detail": "Processed messages successfully."},
+                {"detail": "Processed messages successfully"},
                 status=status.HTTP_201_CREATED,
             )
         except Exception as e:
+            logger.error(f"Error saving batch: {str(e)}", exc_info=True)
             return Response(
                 {"error": f"Database error: {str(e)}"},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
