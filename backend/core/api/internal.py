b881d992cda303c5ce28a5937fba77689ae24cb6
ksiki <j.kolyasha@gmail.com>
2026-06-05 13:48:30 +0300

    feat: add new links (navbar, api, main page)

--- backend/core/api/internal.py
+++ backend/core/api/internal.py
@@ -1,5 +1,5 @@
 from apps.inbox.endpoints import router as inbox_router
-from apps.sources.api.endpoints import router as sources_router
+from apps.sources.endpoints import router as sources_router
 from ninja import NinjaAPI
 
 internal_api = NinjaAPI(
@@ -10,3 +10,4 @@ internal_api = NinjaAPI(
 
 internal_api.add_router("/inbox/", inbox_router)
 internal_api.add_router("/sources/", sources_router)
+internal_api.add_exception_handler
