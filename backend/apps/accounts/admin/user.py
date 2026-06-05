4ca01849da5e632668fec973ca0f5fd243ce746c
ksiki <j.kolyasha@gmail.com>
2026-06-05 13:42:24 +0300

    feat: update admin model for new user

--- backend/apps/accounts/admin/user.py
+++ backend/apps/accounts/admin/user.py
@@ -1,6 +1,5 @@
 from core.admin_mixins import ShortIdAdminMixin
 from django.contrib import admin
-from django.contrib.auth.hashers import make_password
 
 from apps.accounts.models import User
 
@@ -10,6 +9,8 @@ class UserAdmin(ShortIdAdminMixin, admin.ModelAdmin):
     list_display = (
         "short_id",
         "email",
+        "is_active",
+        "is_staff",
         "available_slots",
         "created_at",
     )
@@ -18,15 +19,18 @@ class UserAdmin(ShortIdAdminMixin, admin.ModelAdmin):
     list_editable = ("available_slots",)
 
     search_fields = ("email", "id")
-    list_filter = ("created_at",)
+    list_filter = ("is_active", "is_staff", "is_superuser", "created_at")
 
     list_per_page = 50
     show_full_result_count = False
 
     autocomplete_fields = ("company_blacklist",)
 
+    filter_horizontal = ("groups", "user_permissions")
+
     readonly_fields = (
         "id",
+        "last_login",
         "created_at",
         "updated_at",
     )
@@ -39,6 +43,18 @@ class UserAdmin(ShortIdAdminMixin, admin.ModelAdmin):
             },
         ),
         (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (
             "Slots and Restrictions",
             {
                 "fields": ("available_slots", "company_blacklist"),
@@ -47,17 +63,14 @@ class UserAdmin(ShortIdAdminMixin, admin.ModelAdmin):
         (
             "System Data",
             {
-                "fields": ("id", "created_at", "updated_at"),
+                "fields": ("id", "last_login", "created_at", "updated_at"),
                 "classes": ("collapse",),
             },
         ),
     )
 
     def save_model(self, request, obj, form, change):
-        """
-        Password hashing on change
-        """
-
         if not change or "password" in form.changed_data:
-            obj.password = make_password(obj.password)
+            obj.set_password(obj.password)
+
         super().save_model(request, obj, form, change)
