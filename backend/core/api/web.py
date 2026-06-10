6292362416ce7e8b774b9fb73ca96343b0205c57
ksiki <j.kolyasha@gmail.com>
2026-06-10 12:09:36 +0300

    feat: add urls (cooperation, vacancy, system)

--- backend/core/api/web.py
+++ backend/core/api/web.py
@@ -3,20 +3,24 @@ from apps.accounts.web.endpoints import router as accounts_web_router
 from apps.cooperation.api.endpoints import router as cooperation_api_router
 from apps.cooperation.web.endpoints import router as cooperation_web_router
 from apps.navbar.endpoints import router as navbar_router
-from apps.vacancies.endpoints import router as vacancies_router
+from apps.system.endpoints import router as system_api_router
+from apps.vacancies.api.endpoints import router as vacancies_api_router
+from apps.vacancies.web.endpoints import router as vacancies_web_router
 from ninja import NinjaAPI
 from ninja.throttling import AnonRateThrottle, AuthRateThrottle
 
 web_app = NinjaAPI(
     urls_namespace="web",
     docs=None,
-    throttle=[AnonRateThrottle("10/m"), AuthRateThrottle("100/m")],
+    throttle=[AnonRateThrottle("20/m"), AuthRateThrottle("200/m")],
 )
 
-web_app.add_router("", vacancies_router)
+web_app.add_router("", vacancies_web_router)
 web_app.add_router("", accounts_web_router)
 web_app.add_router("/cooperation/", cooperation_web_router)
 web_app.add_router("/navbar/", navbar_router)
 web_app.add_router("/api/", accounts_api_router)
-web_app.add_router("/api/", cooperation_api_router)
+web_app.add_router("/api/cooperation/", cooperation_api_router)
+web_app.add_router("/api/vacancy/", vacancies_api_router)
+web_app.add_router("/api/system/", system_api_router)
 web_app.add_exception_handler
