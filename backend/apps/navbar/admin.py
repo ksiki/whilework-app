d0bea658b6a1c5ef7a53a975680e8aaed95a408c
ksiki <j.kolyasha@gmail.com>
2026-06-11 16:06:11 +0300

    feat: SupportMessage model

--- backend/apps/navbar/admin.py
+++ backend/apps/navbar/admin.py
@@ -1,3 +1,48 @@
+from core.admin_mixins import ShortIdAdminMixin
 from django.contrib import admin
 
-# Register your models here.
+from .models import SupportMessage
+
+
+@admin.register(SupportMessage)
+class SupportMessageAdmin(ShortIdAdminMixin, admin.ModelAdmin):
+    list_display = (
+        "short_id",
+        "title",
+        "author",
+        "created_at",
+    )
+
+    list_filter = (
+        "created_at",
+        "updated_at",
+    )
+
+    search_fields = (
+        "id",
+        "title",
+        "message",
+        "author__email",
+        "author__username",
+    )
+
+    readonly_fields = (
+        "id",
+        "created_at",
+        "updated_at",
+    )
+
+    raw_id_fields = ("author",)
+
+    date_hierarchy = "created_at"
+
+    fieldsets = (
+        ("Main info", {"fields": ("id", "author", "title", "message")}),
+        (
+            "System info",
+            {
+                "fields": ("created_at", "updated_at"),
+                "classes": ("collapse",),
+            },
+        ),
+    )
--- backend/apps/navbar/models.py
+++ backend/apps/navbar/models.py
@@ -1,1 +1,34 @@
+import uuid6
+from core.models import TimeStampedMixin
+from django.db import models
 
+
+class SupportMessage(TimeStampedMixin):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid6.uuid7,
+        editable=False,
+        verbose_name="Support Message ID",
+    )
+
+    author = models.ForeignKey(
+        "accounts.User",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="support_messages",
+    )
+
+    title = models.CharField(max_length=250, blank=True)
+    message = models.TextField(
+        blank=True,
+    )
+
+    class Meta:
+        db_table = "navbar_support_message"
+        verbose_name = "SupportMessage"
+        verbose_name_plural = "SupportMessages"
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"Support Message {self.id} on User: {self.author.id}"
